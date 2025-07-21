649f94b5293d5eabb16a417ec7219dce66ed5ced
Moses Hika <moseshika811@gmail.com>
2025-07-21 22:35:28 +0000

    Error: Service account object must contain a string "project_id" propert

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -3,11 +3,11 @@ import admin from 'firebase-admin';
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      project_id: process.env.FIREBASE_PROJECT_ID,
       client_email: process.env.FIREBASE_CLIENT_EMAIL,
       private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
-    databaseURL: `https://<${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}>.firebaseio.com`,
+    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
   });
 }
 

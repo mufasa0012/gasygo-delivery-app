6fa36f4b0ab0f35c80680a928b9bd27322ee0ce8
Moses Hika <moseshika811@gmail.com>
2025-07-24 15:28:24 +0000

    add this // Import the functions you need from the SDKs you need import

---
+++ src/lib/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD-O1m3ERCCAW1RvUVqOWNtDBLk8itPJro",
+  authDomain: "pumaly-milk.firebaseapp.com",
+  projectId: "pumaly-milk",
+  storageBucket: "pumaly-milk.appspot.com",
+  messagingSenderId: "435321479368",
+  appId: "1:435321479368:web:4050d57ee778615450c5fd",
+  measurementId: "G-CHD2QVPKKQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export { app };

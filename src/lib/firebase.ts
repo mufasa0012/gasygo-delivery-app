7c689a0eac73443c3611b24e2c62f3a080f3df00
Moses Hika <moseshika811@gmail.com>
2025-07-30 14:13:01 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,25 +2,29 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD-O1m3ERCCAW1RvUVqOWNtDBLk8itPJro",
-  authDomain: "pumaly-milk.firebaseapp.com",
-  projectId: "pumaly-milk",
-  storageBucket: "pumaly-milk.appspot.com",
-  messagingSenderId: "435321479368",
-  appId: "1:435321479368:web:4050d57ee778615450c5fd",
-  measurementId: "G-CHD2QVPKKQ"
+  apiKey: "AIzaSyDwGe_wu3VS0HlaQ3TPHRAcwu4qKj679wA",
+  authDomain: "gasygo-88285.firebaseapp.com",
+  projectId: "gasygo-88285",
+  storageBucket: "gasygo-88285.appspot.com",
+  messagingSenderId: "622199370532",
+  appId: "1:622199370532:web:e3d64dfd509becf1f179b1",
+  measurementId: "G-TK3WJTE6S1"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
+// It's good practice to check if window is defined before calling getAnalytics
+// to avoid errors during server-side rendering.
+const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null;
 
 
-export { app, db, storage };
+export { app, db, storage, analytics };

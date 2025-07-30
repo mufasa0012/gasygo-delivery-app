7a16f00ec9fb323a4cbdf79632b2db1c7e3b87a4
Moses Hika <moseshika811@gmail.com>
2025-07-30 14:30:12 +0000

    Error: [2025-07-30T14:29:02.684Z]  @firebase/firestore: "Firestore (11.9

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,28 +3,22 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDwGe_wu3VS0HlaQ3TPHRAcwu4qKj679wA",
-  authDomain: "gasygo-88285.firebaseapp.com",
-  projectId: "gasygo-88285",
-  storageBucket: "gasygo-88285.appspot.com",
-  messagingSenderId: "622199370532",
-  appId: "1:622199370532:web:e3d64dfd509becf1f179b1",
-  measurementId: "G-TK3WJTE6S1"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
-// It's good practice to check if window is defined before calling getAnalytics
-// to avoid errors during server-side rendering.
 const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null;
-
 
 export { app, db, storage, analytics };
